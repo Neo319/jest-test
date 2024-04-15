c1aa25bb6debcd48c92461d3d0708cb71c8b08d6
CastorDisaster <alexmnevins@gmail.com>
2024-04-15 15:25:33 -0400

    complete reverseString

--- sum.test.js
+++ sum.test.js
@@ -4,8 +4,3 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-const capitalize = require('./capitalize');
-test('first letter should be capitalized', () => {
-    expect(capitalize('jovial')).toBe('Jovial');
-});
-
