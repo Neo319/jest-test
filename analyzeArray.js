823acfd395c2da51934303d9e46e1b0d482b0a6c
CastorDisaster <alexmnevins@gmail.com>
2024-04-17 14:22:21 -0400

    analyzeArray finds average

--- analyzeArray.js
+++ analyzeArray.js
@@ -1,9 +1,16 @@
 export default function analyzeArray (input) {
     
-    
+    function findAverage() {
+        let sum = 0;
+        for (let i = 0; i < input.length; i++) {
+            sum += input[i];
+        };
+        return Math.floor(sum / input.length);
+    }
+
     
     return {
-        average: 'test',
+        average: findAverage(),
         min: 'test',
         max: 'test',
         length: 'test'
--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -3,12 +3,35 @@ test('function exists', () => {
     expect(analyzeArray).toBeDefined()
 });
 test('returns an object', () => {
-    expect(analyzeArray()).toBeInstanceOf(Object)
+    expect(analyzeArray([1,2])).toBeInstanceOf(Object)
 });
 test('returns object with four properties', () => {
-    const length = Object.keys(analyzeArray()).length;
+    const length = Object.keys(analyzeArray([1,2])).length;
     expect(length).toEqual(4)
 });
 test('object.average exists', () => {
-    expect(analyzeArray().average).toBeDefined()
+    expect(analyzeArray([1,2]).average).toBeDefined()
 });
+
+//averages
+test('finds average of three numbers', () => {
+    expect(analyzeArray([1, 3, 5]).average).toBe(3)
+});
+test('rounds average down', () => {
+    expect(analyzeArray([1, 3, 4]).average).toBe(2)
+});
+
+
+// //min & max
+// test('finds max of three numbers', () => {
+//     expect(analyzeArray([1, 3, 5]).max).toBe(5)
+// });
+// test('finds min of three numbers', () => {
+//     expect(analyzeArray([1, 3, 5]).min).toBe(1)
+// });
+
+
+// //length
+// test('finds length of five numbers', () => {
+//     expect(analyzeArray([1, 3, 5, 6, 4]).length).toBe(5)  
+// });
