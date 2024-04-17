012904614df1f394d726700bdd6906cc2992a460
CastorDisaster <alexmnevins@gmail.com>
2024-04-17 13:30:20 -0400

    cipher ignores spaces, punctuation and symbols

--- caesarCipher.js
+++ caesarCipher.js
@@ -2,26 +2,43 @@ export default function caesarCipher (input, key) {
     //loop through letters, building a string with each character 
     // being increased by unicode value of key
     
-    let originalUnicode = input.charCodeAt(0);
-    let newUnicode = originalUnicode + key;
+ 
 
     const lengthofAlphabet = 26;
 
-    let isUpperCase = false;
+    let output = '';
+
+    for (let i = 0; i < input.length; i++) {
+        const originalUnicode = input.charCodeAt(i);
+
+        let isUpperCase = false;
+        let newUnicode;
+
+        //check for spaces, punctuation, symbols
+        if (originalUnicode > 64) {
+            newUnicode = originalUnicode + key;
+        } else {
+            newUnicode = originalUnicode
+        }
     
-    if (originalUnicode >= 65 && originalUnicode <= 95) { //uppercase latin alphabet
-        isUpperCase = true;
+        if (originalUnicode >= 65 && originalUnicode <= 95) { //uppercase latin alphabet
+            isUpperCase = true;
+        }
+
+        // uppercase & lowercase wrap
+        if ((isUpperCase && newUnicode > 90) || 
+        (!isUpperCase && newUnicode > 122)) 
+        { 
+            newUnicode -= lengthofAlphabet;
+        }
+
+        
+
+        const newChar = String.fromCharCode(newUnicode);
+        output = output.concat(newChar)
+
     }
 
-    // uppercase & lowercase wrap
-    if ((isUpperCase && newUnicode > 90) || 
-    (!isUpperCase && newUnicode > 122)) 
-    { 
-        newUnicode -= lengthofAlphabet;
-    }
-
-    const newChar = String.fromCharCode(newUnicode);
-
-    return newChar;
+    return output;
 
 }
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -22,15 +22,19 @@ test('cipher wraps from Z to A', () => {
     expect (caesarCipher('Z', 3)).toBe('C')
 });
 
-// test('cipher with key one works on single word', () => {
-//     expect(caesarCipher('merry', 1)).toBe('nfssz')
-// });
-// test('cipher with key one works on multiple capitalized words', () => {
-//     expect(caesarCipher('Glad lad', 1)).toBe('Hmbe mbe')
-// });
-// test('cipher with key 4 works on one word', () => {
-//     expect(caesarCipher('merry', 4)).toBe('qivvc')
-// });
-// test('cipher with key 4 works on multiple words', () => {
-//     expect(caesarCipher('glad lad', 4)).toBe('kpeh peh')
-// });
+test('cipher with key one works on single word', () => {
+    expect(caesarCipher('merry', 1)).toBe('nfssz')
+});
+test('cipher with key one works on multiple capitalized words', () => {
+    expect(caesarCipher('Glad lad', 1)).toBe('Hmbe mbe')
+});
+test('cipher with key 4 works on one word', () => {
+    expect(caesarCipher('merry', 4)).toBe('qivvc')
+});
+test('cipher with key 4 works on multiple words', () => {
+    expect(caesarCipher('glad lad', 4)).toBe('kpeh peh')
+});
+
+test('cipher does not affect punctuation', () => {
+    expect(caesarCipher("Oh, hi there. Hello world!", 4)).toBe("Sl, lm xlivi. Lipps asvph!")
+});
