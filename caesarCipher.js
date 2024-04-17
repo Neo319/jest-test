f22e8ff488a9c23ec827476eedda1fac28a6cd4e
CastorDisaster <alexmnevins@gmail.com>
2024-04-17 12:58:36 -0400

    implement lowercase wrap

--- caesarCipher.js
+++ caesarCipher.js
@@ -4,6 +4,16 @@ export default function caesarCipher (input, key) {
     
     let newUnicode = input.charCodeAt(0);
     newUnicode += key;
+
+    const lengthofAlphabet = 26;
+    
+
+
+    // if greater in the unicode index than 'z': wrap around to 'a'
+    if (newUnicode > 122) { 
+        newUnicode -= lengthofAlphabet
+    }
+
     const newChar = String.fromCharCode(newUnicode);
 
     return newChar;
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -5,14 +5,28 @@ test('cipher exists', () => {
 test('cipher with key one works on single letter a', () => {
     expect(caesarCipher('a', 1)).toBe('b')
 });
-// test('cipher with key one works on single letter b', () => {
-//     expect(caesarCipher('b', 1)).toBe('c')
-// });
+test('cipher with key two works on single letter b', () => {
+    expect(caesarCipher('b', 2)).toBe('d')
+});
+test('cipher wraps from z to a', () => {
+    expect(caesarCipher('z', 1)).toBe('a')
+});
+test('capitals are maintained', () => {
+    expect(caesarCipher('A', 1)).toBe('B')
+});
+test('cipher wrap works with key > 1', () => {
+    expect(caesarCipher('y', 4)).toBe('c')
+});
+
+test('cipher wraps from Z to A', () => {
+    expect (caesarCipher('Z', 3)).toBe('C')
+});
+
 // test('cipher with key one works on single word', () => {
 //     expect(caesarCipher('merry', 1)).toBe('nfssz')
 // });
-// test('cipher with key one works on multiple words', () => {
-//     expect(caesarCipher('glad lad', 1)).toBe('hmbe mbe')
+// test('cipher with key one works on multiple capitalized words', () => {
+//     expect(caesarCipher('Glad lad', 1)).toBe('Hmbe mbe')
 // });
 // test('cipher with key 4 works on one word', () => {
 //     expect(caesarCipher('merry', 4)).toBe('qivvc')
