e4f3f655ce54a3dc84ca9b0b5badc2e6012c042f
CastorDisaster <alexmnevins@gmail.com>
2024-04-17 13:09:44 -0400

    implement uppercase and lowercase wrap

--- caesarCipher.js
+++ caesarCipher.js
@@ -2,16 +2,22 @@ export default function caesarCipher (input, key) {
     //loop through letters, building a string with each character 
     // being increased by unicode value of key
     
-    let newUnicode = input.charCodeAt(0);
-    newUnicode += key;
+    let originalUnicode = input.charCodeAt(0);
+    let newUnicode = originalUnicode + key;
 
     const lengthofAlphabet = 26;
+
+    let isUpperCase = false;
     
+    if (originalUnicode >= 65 && originalUnicode <= 95) { //uppercase latin alphabet
+        isUpperCase = true;
+    }
 
-
-    // if greater in the unicode index than 'z': wrap around to 'a'
-    if (newUnicode > 122) { 
-        newUnicode -= lengthofAlphabet
+    // uppercase & lowercase wrap
+    if ((isUpperCase && newUnicode > 90) || 
+    (!isUpperCase && newUnicode > 122)) 
+    { 
+        newUnicode -= lengthofAlphabet;
     }
 
     const newChar = String.fromCharCode(newUnicode);
