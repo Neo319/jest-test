ebb6ae4d0151783809e5529d975009c8e932a307
CastorDisaster <alexmnevins@gmail.com>
2024-04-17 11:07:06 -0400

    convert single character using key

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,3 +1,11 @@
-export default function caesarCipher (string, key) {
+export default function caesarCipher (input, key) {
+    //loop through letters, building a string with each character 
+    // being increased by unicode value of key
     
+    let newUnicode = input.charCodeAt(0);
+    newUnicode += key;
+    const newChar = String.fromCharCode(newUnicode);
+
+    return newChar;
+
 }
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -2,15 +2,21 @@ import caesarCipher from './caesarCipher';
 test('cipher exists', () => {
     expect(caesarCipher).toBeDefined()
 });
-test('cipher with key one works on single word', () => {
-    expect(caesarCipher('merry', 1).toBe('nfssz'))
+test('cipher with key one works on single letter a', () => {
+    expect(caesarCipher('a', 1)).toBe('b')
 });
-test('cipher with key one works on multiple words', () => {
-    expect(caesarCipher('glad lad', 1).toBe('hmbe mbe'))
-});
-test('cipher with key 4 works on one word', () => {
-    expect(caesarCipher('merry', 4).toBe('qivvc'))
-});
-test('cipher with key 4 works on multiple words', () => {
-    expect(caesarCipher('glad lad', 4).toBe('kpeh peh'))
-});
+// test('cipher with key one works on single letter b', () => {
+//     expect(caesarCipher('b', 1)).toBe('c')
+// });
+// test('cipher with key one works on single word', () => {
+//     expect(caesarCipher('merry', 1)).toBe('nfssz')
+// });
+// test('cipher with key one works on multiple words', () => {
+//     expect(caesarCipher('glad lad', 1)).toBe('hmbe mbe')
+// });
+// test('cipher with key 4 works on one word', () => {
+//     expect(caesarCipher('merry', 4)).toBe('qivvc')
+// });
+// test('cipher with key 4 works on multiple words', () => {
+//     expect(caesarCipher('glad lad', 4)).toBe('kpeh peh')
+// });
